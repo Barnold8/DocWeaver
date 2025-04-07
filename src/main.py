b2df39c5698d278c6ca6df41b37813356516fff1
Barnold88 <bandslover@outlook.com>
2025-04-07 22:12:48 +0100

    Change file name due to testingd

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
         
     PATH = file_path = os.path.realpath(__file__).split("main.py")[0]
 
-    API_KEY = loadApiKey(PATH+"API_KEY_WRONG")
+    API_KEY = loadApiKey(PATH+"API_KEY")
 
     print(geminiRequest(API_KEY))
