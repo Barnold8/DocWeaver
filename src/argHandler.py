9bd398d0009e19067a4819e8c047b91c7e013f8e
Barnold88 <bandslover@outlook.com>
2025-04-08 19:41:18 +0100

    Add: simple arghandler for user compatability

---
+++ src/argHandler.py
@@ -0,0 +1,38 @@
+import argparse
+
+class Args:
+
+    def __init__(self,size: int,path: str, patterns: list[str],api_key):
+        
+        self.size = size
+        self.path = path
+        self.patterns = patterns
+        self.API_KEY = api_key
+
+
+    def debugPrint(self):
+
+        print(
+            f"Args:\n\n\tsize: {self.args}\n\tpath: {self.path}\n\tpatterns: {self.patterns}"
+        )
+
+def argInit() -> argparse.Namespace:
+    parser =  argparse.ArgumentParser()
+    parser.add_argument("-s","--size", help="decides character limit on file for if it should be split into chunks or not [DEFAULT 1064].",type=int,default=1064)
+    parser.add_argument("-p","--path", help="the file path to the code directory to add docstrings to [DEFAULT CURRENT DIRECTORY].",default="")
+    parser.add_argument("-r","--regex",help="regex pattern to determine the files to add, this can also be included to only include specific directories too [DEFAULT ALLOWS ALL FILES AND PATHS]\n\nUsing -r <regexpattern> -r <regexpattern> will result in the current found filepath to be accepted if it matches to either pattern.",nargs='+',default=[])
+    parser.add_argument("-a","--api",help="the path to the location of your API key [DEFAULT IS THE CURRENT PATH + API_KEY]",default="API_KEY")
+    return parser.parse_args()
+
+def handleArgs(args)-> Args:
+
+    parser = argInit()
+
+    return Args(
+        parser.size,
+        parser.path,
+        parser.regex,
+        parser.api
+    )
+
+     
