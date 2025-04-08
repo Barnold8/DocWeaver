33eadf655a092e5578060f3083bdd6a5a5d3e212
Barnold88 <bandslover@outlook.com>
2025-04-08 20:50:39 +0100

    Fix argHandler to support multiple regex patterns

--- src/argHandler.py
+++ src/argHandler.py
@@ -25,13 +25,15 @@ def argInit() -> argparse.Namespace:
     parser =  argparse.ArgumentParser()
     parser.add_argument("-s","--size", help="decides character limit on file for if it should be split into chunks or not [DEFAULT 1064].",type=int,default=1064)
     parser.add_argument("-p","--path", help="the file path to the code directory to add docstrings to [DEFAULT CURRENT DIRECTORY].",default="")
-    parser.add_argument("-r","--regex",help="regex pattern to determine the files to add, this can also be included to only include specific directories too [DEFAULT ALLOWS ALL FILES AND PATHS]\n\nUsing -r <regexpattern> -r <regexpattern> will result in the current found filepath to be accepted if it matches to either pattern.",nargs='+',default=[])
+    parser.add_argument("-r","--regex",help="regex pattern to determine the files to add, this can also be included to only include specific directories too [DEFAULT ALLOWS ALL FILES AND PATHS]\n\nUsing -r <regexpattern> -r <regexpattern> will result in the current found filepath to be accepted if it matches to either pattern.",default=[],action='append')
     parser.add_argument("-a","--api",help="the path to the location of your API key [DEFAULT IS THE CURRENT PATH + API_KEY]",default="API_KEY")
     return parser.parse_args()
 
 def handleArgs()-> Args:
 
     parser = argInit()
+
+    print(parser.regex)
 
     return Args(
         parser.size,
